678cd28efbd21db39558b41199a220cf13266dc9
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-11 17:00:33 -0800

    Refactored okay_two_sum? to work with bsearch

--- w4d5/TwoSumProblem/two_sum_problem.rb
+++ w4d5/TwoSumProblem/two_sum_problem.rb
@@ -9,26 +9,38 @@ def two_sum?(arr, target_sum)
 end
 
 #big O
-#n^2
+#O(n^2)
 
 
 
 def okay_two_sum?(arr, target_sum)
   arr.sort!                          #O(n log n)
-  return false if arr.length == 1
-  first_num = arr.pop
-  if arr.any? {|num| num + first_num == target_sum}     #O(n)
-    return true
-  else
-    okay_two_sum?(arr, target_sum)        #O(log n)
+  until arr.empty?
+    num = arr.pop
+    return true if bsearch(arr, target_sum - num)
   end
-
-  # poss_sum = arr[arr.length/2 - 1] + arr[arr.length/2]
-  # return true if poss_sum == target_sum
-  # okay_two_sum?(arr[0...arr.length/2], target_sum) || okay_two_sum?(arr[arr.length/2..-1], target_sum)
+  false
 end
 #big O = O(n log n) = n log n + n + (log n)
+
+def bsearch(arr, target)
+  return false if arr.length == 0
+
+  mid = arr.length/2
+
+  return mid if arr[mid] == target
+  if arr[mid] > target
+    found = bsearch(arr[mid+1..-1], target)
+    found + mid + 1 unless found == false
+  else
+    bsearch(arr[0...mid], target)
+  end
+  false
+end
+
 arr = [0, 1, 5, 7]
+# p bsearch(arr,5)
+# p bsearch(arr, 0)
 p okay_two_sum?(arr, 6) # => should be true
 p okay_two_sum?(arr, 10) # => should be false
 # Sorting
@@ -38,3 +50,39 @@ p okay_two_sum?(arr, 10) # => should be false
 # How might sorting that make the problem easier?
 # Write a second solution, called okay_two_sum?, which uses sorting. Make sure it works correctly.
 # Hint: (There are a couple ways to solve this problem once it's sorted. One way involves using a very cheap algorithm that can only be used on sorted data sets. What are some such algorithms you know?)
+
+class HashMap
+  attr_reader :hashmap
+  def initialize(arr)  # can pass it an array and a default value
+    @hashmap = {}
+    i = 0
+    arr.each { |ele| @hashmap[i] = ele ; i += 1 }
+  end
+
+  def get(index)
+    @hashmap[index]
+  end
+
+  def set(index, value)
+    @hash_map[index] = value
+  end
+end
+
+
+# def hash_two_sum?(arr, target)
+#   hash1 = HashMap.new(arr)
+#   (0...arr.length).each do |i|
+#     hash1.get(i) + hash1.get(i+1) == target
+#   end
+# end
+
+
+
+# arr = [1,2,3,4]
+# hash_two_sum?(arr, 6)
+
+
+# arr[3] => 4
+
+# hash[3] => 4
+
