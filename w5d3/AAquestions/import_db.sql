e02e618f9178eefd8abfe2eb3bed82ac98a1dfc1
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-16 14:07:11 -0800

    Added project files from w5d1 and w5d2
    Began work on w5d3 projects

---
+++ w5d3/AAquestions/import_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users(
+        id INTEGER PRIMARY KEY,
+        fname TEXT NOT NULL,
+        lname TEXT NOT NULL
+    );
+
+CREATE TABLE questions(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        body TEXT NOT NULL,
+        author_id INTEGER NOT NULL,
+        FOREIGN KEY (author_id) REFERENCES users(id)
+    );
+
+
+    
