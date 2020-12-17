1aae095f52790ed14291b46d0011127d1c4a2012
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-16 16:02:42 -0800

    Added class for Questions table manipulation

--- w5d3/AAquestions/questions_database.rb
+++ w5d3/AAquestions/questions_database.rb
@@ -2,6 +2,14 @@ require 'sqlite3'
 
 class QuestionsDataBase < SQLite3::Database
     include Singleton 
+    def initialize
+        super('questions.db')
+        self.type_translation = true
+        self.results_as_hash = true
+    end
+end
+
+class Question
     def self.find_by_id(find_id)
         id = execute(<<-SQL)
             SELECT
@@ -13,11 +21,13 @@ class QuestionsDataBase < SQLite3::Database
             WHERE
                 id = find_id
         SQL
-        Question.new(id.values)
+        Question.new(*id.values)
     end
-end
 
-class Question
-    def initialize
+    attr_accessors :title, :body, :author
+    def initialize(title, body, author)
+        @title = title
+        @body = body
+        @author = author
     end
 end
