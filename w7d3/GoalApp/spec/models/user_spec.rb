e2668fddb74c35fbf1ceaddb3d2ed304e66481e3
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-06 15:02:37 -0800

    Began writing tests for User model

---
+++ w7d3/GoalApp/spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+# rpsec is connected to the USER MODEL
+RSpec.describe User, type: :model do
+
+  it {should validate_presence_of(:username)}
+  it {should validate_presence_of(:password_digest)}
+  it {should validate_length_of(:password).is_at_least(6)}
+
+  
+end
