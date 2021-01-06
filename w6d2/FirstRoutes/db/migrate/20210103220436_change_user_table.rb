6285f1b53b77cec8b7347ac2a8b2a0d85b03444c
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-06 13:38:33 -0800

    added all files to be pushed and fix the repo

---
+++ w6d2/FirstRoutes/db/migrate/20210103220436_change_user_table.rb
@@ -0,0 +1,8 @@
+class ChangeUserTable < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users do |t|
+      t.remove :name, :email
+      t.string :username, null: false, unique: true
+    end
+  end
+end
