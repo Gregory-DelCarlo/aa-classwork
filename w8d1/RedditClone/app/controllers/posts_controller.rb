982462f8f86199148b8f21a60259319e34383ee5
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-13 12:06:27 -0800

    pushed work for Rails Lite-mAdded project files for first javascript project

--- w8d1/RedditClone/app/controllers/posts_controller.rb
+++ w8d1/RedditClone/app/controllers/posts_controller.rb
@@ -27,6 +27,6 @@ class PostsController < ApplicationController
 
     private
     def post_params
-        params.require(:post).permit(:title, :url, :sub_id, :author_id)
+        params.require(:post).permit(:title, :url, :sub_id, :author_id) 
     end
 end
