0bd670b5a387975776d23e7050e277ab32be31ef
AlexeyS <xengawe@gmail.com>
2021-01-13 16:19:30 -0800

    complete myMap

--- w8d3/javascript_intro/phase_2_enumerable.js
+++ w8d3/javascript_intro/phase_2_enumerable.js
@@ -4,18 +4,31 @@ Array.prototype.myEach = function(cb) {
     }
 }
 
-let arr1 = [];
+// let arr1 = [];
 // block being called by myEach
-let myfunc = function(num) {
+let myfunc1 = function(num) {
     // for (let i =0; i < arr.length; i++) {
-    arr1.push(num*2);
+    return num * 2;
     // }
 }
 
-let arr = [1,2,3,4];
-arr.myEach(myfunc);
-arr.myEach( function(num) { console.log(num) } )
 // arr.each do => call to myEach
 // |ele|        => this[i] or myfunc = function(num)
 // print("hello") => arr1.push(num*2)
 // end
+
+Array.prototype.myMap = function(cb1) {
+    let newarr = [];
+    this.myEach(function(num) {
+        newarr.push(cb1(num));    
+    }
+    )
+    return newarr;
+}
+    
+    
+let arr = [1,2,3,4];
+console.log(arr.myMap(myfunc1));
+// arr.myEach( function(num) { console.log(num) } )
+    
+    
