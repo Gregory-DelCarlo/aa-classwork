1e3aecdb99f5f811c0d4cd0938e08535959c69e1
kylemuch <kylemuch@gmail.com>
2021-01-15 16:20:26 -0800

    finished checkvert/horizontal

--- w8d5/ttt_JS/board.js
+++ w8d5/ttt_JS/board.js
@@ -3,15 +3,65 @@
 class Board {
     constructor() {
         this.grid = [
+            ['X', undefined, undefined],
             [undefined, undefined, undefined],
-            [undefined, undefined, undefined],
-            [undefined, undefined, undefined]
+            ['X', undefined, undefined]
         ]
     }
 
     won() {
         continue;
     }
+
+    _checkHorizontals(marker) {
+        let column = [];
+        let win = false;
+
+        for (let j = 0; j < 3; j++) {
+            for (let i = 0; i < 3; i++) {
+                win = true;
+                column.push(this.grid[j][i]);
+            }
+            column.forEach(el => {
+                if (el !== marker) {
+                    win = false;
+                }
+            });
+
+            if (win === true) {
+                return true;
+            } else {
+                column = [];
+            }
+        }
+
+        return false;
+    }
+    _checkVerticals(marker) {
+        let column = [];
+        let win = false;
+
+        for (let j = 0; j < 3; j++) {
+            for (let i = 0; i < 3; i++) {
+                win = true;
+                column.push(this.grid[i][j]);
+            }
+            column.forEach(el => {
+                if (el !== marker) {
+                    win = false;
+                }
+            });
+
+            if (win === true) {
+                return true;
+            } else {
+                column = [];
+            }
+        }
+
+        return false;
+    }
+    _checkDiagonals() {}
 
     winner() {
         continue;
@@ -34,3 +84,6 @@ class Board {
     }
 
 }
+
+let b = new Board();
+console.log(b._checkHorizontals('X'));
