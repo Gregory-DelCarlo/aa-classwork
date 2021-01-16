2f363fc9870b3028821cf411555b632caf9f8d21
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-15 16:59:06 -0800

    Refactored and fixed bugs in our TTT row check helpers

--- w8d5/ttt_JS/board.js
+++ w8d5/ttt_JS/board.js
@@ -3,9 +3,9 @@
 class Board {
     constructor() {
         this.grid = [
-            ['X', undefined, undefined],
             [undefined, undefined, undefined],
-            ['X', undefined, undefined]
+            [undefined, undefined, undefined],
+            [undefined, undefined, undefined]
         ]
     }
 
@@ -14,54 +14,67 @@ class Board {
     }
 
     _checkHorizontals(marker) {
-        let column = [];
-        let win = false;
-
+        let win = true;
+        
         for (let j = 0; j < 3; j++) {
+            let column = [];
             for (let i = 0; i < 3; i++) {
                 win = true;
                 column.push(this.grid[j][i]);
             }
-            column.forEach(el => {
-                if (el !== marker) {
-                    win = false;
-                }
-            });
-
+            win = this._checkSet(column, marker)
             if (win === true) {
-                return true;
-            } else {
-                column = [];
+                break;
             }
         }
 
-        return false;
+        return win;
     }
     _checkVerticals(marker) {
-        let column = [];
-        let win = false;
-
+        let win = true;
+        
         for (let j = 0; j < 3; j++) {
+            let row = [];
             for (let i = 0; i < 3; i++) {
                 win = true;
-                column.push(this.grid[i][j]);
+                row.push(this.grid[i][j]);
             }
-            column.forEach(el => {
-                if (el !== marker) {
-                    win = false;
-                }
-            });
-
+            // console.log(row)
+            win = this._checkSet(row, marker)
             if (win === true) {
-                return true;
-            } else {
-                column = [];
+                break;
             }
         }
 
-        return false;
+        return win;
     }
-    _checkDiagonals() {}
+
+    _checkDiagonals(marker) {
+        let leftDiag = [];
+        let rightDiag = [];
+
+        for(let i = 0; i < 3; i++) {
+            leftDiag.push(this.grid[i][i])
+            rightDiag.push(this.grid[i][-i-1])
+        }
+        let win = true;
+        win = this._checkSet(leftDiag, marker)
+        if (win === false) {
+            win = this._checkSet(rightDiag, marker)
+        }
+        return win;
+    }
+
+    _checkSet(set, marker) {
+        let win = true;
+        set.forEach(el => {
+            if (el !== marker) {
+                win = false;
+            }
+        });
+
+        return win;
+    }
 
     winner() {
         continue;
@@ -83,7 +96,108 @@ class Board {
         continue;
     }
 
+    changeGrid(grid) {
+        this.grid = grid;
+    }
+
 }
 
-let b = new Board();
-console.log(b._checkHorizontals('X'));
+// let vb1 = [
+//     [undefined, "X",undefined],
+//     [undefined, "X", undefined],
+//     [undefined, "X", undefined]
+// ];
+
+// let vb2 = [
+//     [undefined, "X",undefined],
+//     ["X", undefined, undefined],
+//     [undefined, "X", undefined]
+// ];
+
+// let vb3 = [
+//     [undefined, "X",undefined],
+//     [undefined, "O", undefined],
+//     [undefined, "X", undefined]
+// ]
+
+// let hb1 = [
+//     [undefined, undefined, undefined],
+//     [undefined, undefined, undefined],
+//     ["X", "X", "X"]
+// ];
+
+// let hb2 = [
+//     [undefined, undefined, undefined],
+//     [undefined, "X", undefined],
+//     ["X", undefined, "X"]
+// ];
+
+// let hb3 = [ 
+//     [undefined, undefined, undefined],
+//     [undefined, undefined, undefined],
+//     ["X", "O", "X"]
+// ]
+
+// let db1 = [
+//     ["X", undefined, undefined],
+//     [undefined, "X", undefined],
+//     [undefined, undefined, "X"]
+// ];
+
+// let db2 = [
+//     ["X", undefined, undefined],
+//     ["X", undefined, undefined],
+//     [undefined, undefined, "X"]
+// ];
+
+// let db3 = [
+//     ["X", undefined, undefined],
+//     [undefined, "O", undefined],
+//     [undefined, undefined, "X"]
+// ]
+
+// let b = new Board();
+// b.changeGrid(hb1);
+// console.log(b.grid);
+// console.log("Expected Output: true");
+// console.log(b._checkHorizontals('X'));
+
+// b.changeGrid(hb2);
+// console.log(b.grid);
+// console.log("Expected Output: false");
+// console.log(b._checkHorizontals('X'));
+
+// b.changeGrid(hb3);
+// console.log(b.grid);
+// console.log("Expected Output: false");
+// console.log(b._checkHorizontals('X'));
+
+// b.changeGrid(vb1);
+// console.log(b.grid);
+// console.log("Expected Output: true");
+// console.log(b._checkVerticals('X'));
+
+// b.changeGrid(vb2);
+// console.log(b.grid);
+// console.log("Expected Output: false");
+// console.log(b._checkVerticals('X'));
+
+// b.changeGrid(vb3);
+// console.log(b.grid);
+// console.log("Expected Output: false");
+// console.log(b._checkVerticals('X'));
+
+// b.changeGrid(db1);
+// console.log(b.grid);
+// console.log("Expected Output: true");
+// console.log(b._checkDiagonals('X'));
+
+// b.changeGrid(db2);
+// console.log(b.grid);
+// console.log("Expected Output: false");
+// console.log(b._checkDiagonals('X'));
+
+// b.changeGrid(db3);
+// console.log(b.grid);
+// console.log("Expected Output: false");
+// console.log(b._checkDiagonals('X'));
