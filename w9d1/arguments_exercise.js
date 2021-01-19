28131d62fa29cdb3069e5b875b5bad699053f059
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-19 14:48:32 -0800

    Finished myBind and played with multiple way to make it work

--- w9d1/arguments_exercise.js
+++ w9d1/arguments_exercise.js
@@ -1,25 +1,111 @@
 //Greg, I did some of these earlier ones that you might find really easy, but I still need to work on them. I stopped at the myBind with calltime and bindtime args. 
 
 
-Function.prototype.myBind = function (context, ...bindArgs) {
+// Function.prototype.myBind = function (context, ...bindArgs) {
+//   const that = this;
+//   return function (...callArgs) {
+//     return that.apply(context, bindArgs.concat(callArgs));
+//   };
+// };
+
+
+Function.prototype.myBind = function(context) {
   const that = this;
-  return function (...callArgs) {
-    return that.apply(context, bindArgs.concat(callArgs));
+  let args = Array.prototype.slice.call(arguments);
+  args.shift();
+  return function() {
+    let callArgs = Array.prototype.slice.call(arguments);
+    return that.apply(context, args.concat(callArgs));
   };
 };
 
-// arguments way 
-// function sum(){
-//   let args = Array.prototype.slice.call(arguments);
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
 
-//   args.forEach((arg) => {
-//     console.log(arg);
-//   });
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+
+  _addFood (food1, food2) {
+    console.log(`You made a ${food1} ${food2}`)
+  }
+}
+
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Human {
+  constructor(name) {
+    this.name = name;
+    this.occupation = "unemployed";
+  }
+
+  destroy(thing) {
+    console.log(`${this.name} destroys ${thing} becuase ${this.occupation}`)
+  }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+const Joe = new Human("Joe");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+markov._addFood("tuna", "salad");
+markov._addFood.myBind(Joe, "pizza")("pie");
+
+markov.says.myBind(Joe)("yells", "George");
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
+
+
+
+
+
+
+
+// arguments way 
+// function sum(cb){
+//   let args = Array.prototype.slice.call(arguments);
+//   args.shift();
+//   let sum = args.reduce(cb);
+//   // args.forEach((arg) => {
+//   //   console.log(arg);
+//   //   sum += arg
+//   // });
+//  return sum;
 // }
 
-//reducer callback to pass to reduce method
+// reducer callback to pass to reduce method
 // let reducer = (accumulator, currentValue) => accumulator + currentValue;
-// rest way
+// // rest way
 // function sum(cb, operOne,...otherOpers) {
 
 //   console.log("operOne: " + operOne);
