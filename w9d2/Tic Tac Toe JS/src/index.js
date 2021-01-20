801f0325d794ccc6f788994e65015323bb50fce4
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-20 13:55:20 -0800

    worked on getting our TTT board drawn on the screen

--- w9d2/Tic Tac Toe JS/src/index.js
+++ w9d2/Tic Tac Toe JS/src/index.js
@@ -1,5 +1,5 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js")  // require appropriate file
+const Game = require("../dist/game.js")// require appropriate file
 
   $(() => {
     // Your code here
--- w9d2/Tic Tac Toe JS/src/ttt-view.js
+++ w9d2/Tic Tac Toe JS/src/ttt-view.js
@@ -1,11 +1,31 @@
+const Game = require("../dist/game");
+
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $($el);
+    this.setupBoard();
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+   $(
+     () => {
+      const $newBoard = $('<ul class="grid"></ul>');
+      for (let i = 0; i < 9; i++) {
+        $newBoard.append('<li class="grid-box"></li>');
+      };
+      // need to append to $el within the method or the return breaks
+      this.$el.append($newBoard);
+     });
+  }
 }
 
 module.exports = View;
+
+$( () =>{
+  new View(new Game(), ".ttt");
+});
