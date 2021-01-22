53eab46f7db253bcae07cceafed0f4308acd7223
Bogey <jeffreysbogart@gmail.com>
2021-01-21 16:02:50 -0800

    Adds api_util file

--- w9d3/twitterAjax/app/assets/javascripts/bundle.js
+++ w9d3/twitterAjax/app/assets/javascripts/bundle.js
@@ -41,13 +41,23 @@ class FollowToggle {
     constructor (e) {
         this.$e = $(e);
         this.userId = this.$e.data("user-id");
-        this.followState = this.$e.data("initial-follow-state");
+        if (this.$e.data("initial-follow-state")) {
+            this.followState = "followed";
+        } else {
+            this.followState = "unfollowed";
+        }
         this.render();
         this.handleClick();
     }
 
     render() {
-        if (this.followState === false){
+        if (this.followState === "unfollowing" || this.followState === "following") {
+            this.$e.prop("disabled", true);
+        } else {
+            this.$e.prop("disabled", false);
+        }
+
+        if (this.followState === "unfollowed" || this.followState === "unfollowing"){
             this.$e.text("Follow!");
         }else {
             this.$e.text("Unfollow!");
@@ -57,32 +67,23 @@ class FollowToggle {
     handleClick() {
         this.$e.on("click", (e) => {
             e.preventDefault();
-            if (this.followState) {
+            if (this.followState === "followed") {
                 APIUtil.unfollowUser(this.userId).then(() => {
-                    this.followState = false;
+                    this.followState = "unfollowing";
                     this.render();
                 }).fail(() => {console.log("Didn't follow/unfollow")});
+                this.followState = "unfollowed";
+                this.render();
             } else {
                 APIUtil.followUser(this.userId).then(() => {
-                    this.followState = true;
+                    this.followState = "following";
                     this.render();
                 }).fail(() => {console.log("Didn't follow/unfollow")});
+                this.followState = "followed";
+                this.render();
             };
         });
     }
-    // return $.ajax({
-    //     method: method,
-    //     url: `/users/${this.userId}/follow`, // use string interpolation to add the userID to our path
-    //     dataType: "JSON"
-    // }).then(success => {
-    //     if (method === "POST") {
-    //         this.followState = true;
-    //     } else if (method === "DELETE") {
-    //         this.followState = false;
-    //     }
-    //     this.render();
-    // }).fail(failure => {
-    //     console.log("Didn't follow/unfollow")
 }
 
 module.exports = FollowToggle;
