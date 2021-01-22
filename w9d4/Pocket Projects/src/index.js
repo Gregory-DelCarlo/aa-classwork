2fb9a43fdf013404cb83a34a64b4c4c2beaf84f9
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-22 14:21:37 -0800

    Began working on the warmup problem
    need to get it working inside index.js

--- w9d4/Pocket Projects/src/index.js
+++ w9d4/Pocket Projects/src/index.js
@@ -1,2 +1,5 @@
-import warmUp from "./warmup";
+import htmlGenerator from "./warmup";
+
+const partyHeader = document.getElementById('party');
+htmlGenerator("Vannilla DOM!!", partyHeader);
 
--- w9d4/Pocket Projects/src/warmup.js
+++ w9d4/Pocket Projects/src/warmup.js
@@ -2,7 +2,11 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+    const p = document.createElement("p");
+    p.textContent = string;
+    htmlElement.appendChild(p)
 };
 
-htmlGenerator('Party Time.', partyHeader);
+// htmlGenerator('Party Time.', partyHeader);
+
+// module.exports = htmlGenerator;
