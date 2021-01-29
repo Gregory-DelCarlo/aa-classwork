01ebdeb76c552396c31a63502b2ea80e2cfbf1f3
kinda-dev <maybe.fabio@gmail.com>
2021-01-29 15:23:05 -0800

    work on ToDo ListContainer on react_redux_todos project

--- w10d5/react_redux_todos_parts_1_and_2/frontend/todo_redux.jsx
+++ w10d5/react_redux_todos_parts_1_and_2/frontend/todo_redux.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {receiveTodo, receiveTodos, removeTodo} from './actions/todo_actions';
-import {receiveStep, receiveSteps, removeStep} from './actions/step_actions';
+// import App from "./components/app"
+// import {receiveTodo, receiveTodos, removeTodo} from './actions/todo_actions';
+// import {receiveStep, receiveSteps, removeStep} from './actions/step_actions';
 import configureStore from "./store/store";
-
+import Root from "./components/root"
+import { allTodos } from './reducers/selectors'
 
 document.addEventListener("DOMContentLoaded",() => {
     const root = document.getElementById("react-root");
-    const H1 = React.createElement('h1', {}, 'To Dos App');
-    ReactDom.render(H1, root);
-
-    window.receiveStep = receiveStep;
-    window.receiveSteps = receiveSteps;
-    window.removeStep = removeStep;
-    window.receiveTodo = receiveTodo;
-    window.receiveTodos = receiveTodos;
-    window.removeTodo = removeTodo;
+    // const H1 = React.createElement('h1', {}, 'To Dos App');
     window.store = configureStore();
+
+
+    ReactDom.render(<Root store={window.store}/>, root);
+
+
+    // testing
+    window.allTodos = allTodos;
+    // window.receiveStep = receiveStep;
+    // window.receiveSteps = receiveSteps;
+    // window.removeStep = removeStep;
+    // window.receiveTodo = receiveTodo;
+    // window.receiveTodos = receiveTodos;
+    // window.removeTodo = removeTodo;
 })
 
