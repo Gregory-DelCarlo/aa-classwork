e7b6e50b68f331db7347a20e8fac0522b4106830
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-29 16:26:50 -0800

    Set up to render todolist

--- w10d5/react_redux_todos_parts_1_and_2/frontend/components/todos/todo_list.jsx
+++ w10d5/react_redux_todos_parts_1_and_2/frontend/components/todos/todo_list.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
 export default () => {
     return <h3>Todo List goes here!</h3>
-}    
+}
+
+export default class TodoList extends React.Component {
+    constructor({todos}) {
+        super(todos);
+    }
+
+    render() {
+        return({
+
+        });
+    }
+}
